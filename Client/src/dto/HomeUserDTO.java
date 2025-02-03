c6add4b66851fc9a38e5427da1ed9ebcf0431852
Mohamed Ashraf <93071951+TmohamedashrafT@users.noreply.github.com>
2025-02-03 19:38:01 +0200

    Create the  Create the HomeUserDTO class (#8)
    * merge (#3)
    
    * Remove the handlers for the text fields in the login scene
    
    * merge (#3) (#4) (#5)
    
    * merge (#3) (#4) (#6)
    
    * Remove the handlers for the text fields in the login scene
    
    * Create the HomeUserDTO class

---
+++ Client/src/dto/HomeUserDTO.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dto;
+
+/**
+ *
+ * @author Ahmed
+ */
+public class HomeUserDTO {
+    private String username;
+    private String fullName;
+    private double balance;
+
+    public HomeUserDTO(String username, String fullName, double balance) {
+        this.username = username;
+        this.fullName = fullName;
+        this.balance = balance;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+
+}
