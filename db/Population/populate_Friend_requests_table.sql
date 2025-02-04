e5be3d36b359bbd6b16a130c0ed8ae24b547def4
Mennatullahatta <https://github.com/Mennatullahatta>
2025-02-04 18:13:01 +0200

    Populate the Friend_requests table

---
+++ db/Population/populate_Friend_requests_table.sql
@@ -0,0 +1,24 @@
+INSERT INTO Friend_requests VALUES ('mohamed', 'jane_smith');
+INSERT INTO Friend_requests VALUES ('somaya', 'emma_white');
+
+INSERT INTO Friend_requests VALUES ('alex_brown', 'sophia_clark');
+INSERT INTO Friend_requests VALUES ('william_hall', 'mia_johnson');
+
+INSERT INTO Friend_requests VALUES ('david_adams', 'olivia_walker');
+INSERT INTO Friend_requests VALUES ('emma_white', 'michael_lee');
+
+INSERT INTO Friend_requests VALUES ('sophia_clark', 'omar');
+INSERT INTO Friend_requests VALUES ('mia_johnson', 'menna');
+
+INSERT INTO Friend_requests VALUES ('somaya', 'alex_brown');
+INSERT INTO Friend_requests VALUES ('michael_lee', 'olivia_walker');
+
+INSERT INTO Friend_requests VALUES ('william_hall', 'emma_white');
+INSERT INTO Friend_requests VALUES ('david_adams', 'sophia_clark');
+
+INSERT INTO Friend_requests VALUES ('mia_johnson', 'david_adams');
+INSERT INTO Friend_requests VALUES ('olivia_walker', 'michael_lee');
+INSERT INTO Friend_requests VALUES ('jane_smith', 'omar');
+
+COMMIT;
+
