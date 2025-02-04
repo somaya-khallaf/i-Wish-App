775385a172d6a77296fefc515f784814769a1941
Mennatullahatta <https://github.com/Mennatullahatta>
2025-02-04 18:29:47 +0200

    Populate the friend table

---
+++ db/Population/populate_friend_table.sql
@@ -0,0 +1,25 @@
+INSERT INTO friend VALUES ('mohamed', 'omar');
+INSERT INTO friend VALUES ('omar', 'mohamed');
+
+INSERT INTO friend VALUES ('mohamed', 'menna');
+INSERT INTO friend VALUES ('menna', 'mohamed');
+
+INSERT INTO friend VALUES ('omar', 'menna');
+INSERT INTO friend VALUES ('menna', 'omar');
+
+INSERT INTO friend VALUES ('somaya', 'menna');
+INSERT INTO friend VALUES ('menna', 'somaya');
+
+INSERT INTO friend VALUES ('jane_smith', 'alex_brown');
+INSERT INTO friend VALUES ('alex_brown', 'jane_smith');
+
+INSERT INTO friend VALUES ('emma_white', 'sophia_clark');
+INSERT INTO friend VALUES ('sophia_clark', 'emma_white');
+
+INSERT INTO friend VALUES ('michael_lee', 'david_adams');
+INSERT INTO friend VALUES ('david_adams', 'michael_lee');
+
+INSERT INTO friend VALUES ('olivia_walker', 'mia_johnson');
+INSERT INTO friend VALUES ('mia_johnson', 'olivia_walker');
+
+COMMIT;
