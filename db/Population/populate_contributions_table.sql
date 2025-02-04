5f767df7ad701dd158571239b00166bc4ebbc0d6
somaya_khallaf <87336258+somaya-khallaf@users.noreply.github.com>
2025-02-04 18:51:05 +0200

    Populate the contributions table

---
+++ db/Population/populate_contributions_table.sql
@@ -0,0 +1,13 @@
+insert into contributions (Contributer_name, wish_id, amount) values ('omar', 1, 100);
+insert into contributions (Contributer_name, wish_id, amount) values ('mohamed', 15, 50);
+
+insert into contributions (Contributer_name, wish_id, amount) values ('somaya', 3, 60);
+insert into contributions (Contributer_name, wish_id, amount) values ('menna', 17, 100);
+
+insert into contributions (Contributer_name, wish_id, amount) values ('alex_brown', 7, 300);
+insert into contributions (Contributer_name, wish_id, amount) values ('emma_white', 19, 150);
+
+insert into contributions (Contributer_name, wish_id, amount) values ('mia_johnson', 25, 500);
+insert into contributions (Contributer_name, wish_id, amount) values ('david_adams', 13, 100);
+
+commit;
