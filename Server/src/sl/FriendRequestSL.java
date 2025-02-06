16d395db44e8d0dc5782d6dcd1a531c90c319586
somaya_khallaf <87336258+somaya-khallaf@users.noreply.github.com>
2025-02-06 14:07:15 +0200

    Update the getFriendRequestList function

--- Server/src/sl/FriendRequestSL.java
+++ Server/src/sl/FriendRequestSL.java
@@ -2,7 +2,7 @@ package sl;
 
 import dto.FriendDTO;
 import dto.UserDTO;
-
+import dal.FriendRequestDAO;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
@@ -11,5 +11,9 @@ public class FriendRequestSL {
     static public int sendFriendRequest(String friend, String userName) throws SQLException {return 0;}
     static public int rejectFriendRequest(String friend, String userName)throws SQLException{return 0;}
     static public int acceptFriendRequest(String friend, String userName) throws SQLException {return 0;}
-    static public ArrayList<FriendDTO> getFriendRequestList(String user) throws SQLException{return new ArrayList<>();}
+    static public ArrayList<FriendDTO> getFriendRequestList(String user) throws SQLException {
+        if(user != null && !user.isEmpty())
+            return FriendRequestDAO.getAllFriendRequests(user);
+        return null;
+    }
 }
