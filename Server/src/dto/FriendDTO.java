7b1fad0793c07fcfc6cf133dcf54e265d29f46cc
somaya_khallaf <87336258+somaya-khallaf@users.noreply.github.com>
2025-02-06 13:19:55 +0200

    Create the FriendDTO class

--- Server/src/dto/FriendDTO.java
+++ Server/src/dto/FriendDTO.java
@@ -1,4 +1,31 @@
 package dto;
 
 public class FriendDTO {
+
+    private String Friendusername;
+    private String Friendfullname;
+
+    public FriendDTO(String Friendusername) {
+        this.Friendusername = Friendusername;
+    }
+    public FriendDTO(String Friendusername, String Friendfullname) {
+        this.Friendusername = Friendusername;
+        this.Friendfullname = Friendfullname;
+    }
+
+    public void setFriendusername(String Friendusername) {
+        this.Friendusername = Friendusername;
+    }
+
+    public void setFriendfullname(String Friendfullname) {
+        this.Friendfullname = Friendfullname;
+    }
+
+    public String getFriendusername() {
+        return Friendusername;
+    }
+
+    public String getFriendfullname() {
+        return Friendfullname;
+    }
 }
