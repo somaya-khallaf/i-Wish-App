2b9f2017063cb49680844182b6464313f90e1d7c
Mohamed Ashraf <mohamed.ashraf211087@gmail.com>
2025-02-07 17:34:43 +0200

    update the HomepageDTO class

--- Client/src/dto/HomePageDTO.java
+++ Client/src/dto/HomePageDTO.java
@@ -3,13 +3,37 @@ package dto;
 import java.util.ArrayList;
 
 public class HomePageDTO {
-    private UserDTO userDTO;
+    private HomeUserDTO userDTO;
     private ArrayList<WishDTO> wishList;
     private ArrayList<NotificationDTO> notificationList;
 
-    public HomePageDTO(UserDTO userDTO, ArrayList<WishDTO> wishList, ArrayList<NotificationDTO> notificationList) {
+    public HomePageDTO(HomeUserDTO userDTO, ArrayList<WishDTO> wishList, ArrayList<NotificationDTO> notificationList) {
         this.userDTO = userDTO;
         this.wishList = wishList;
+        this.notificationList = notificationList;
+    }
+
+    public HomeUserDTO getHomeUserDTO() {
+        return userDTO;
+    }
+
+    public ArrayList<WishDTO> getWishList() {
+        return wishList;
+    }
+
+    public ArrayList<NotificationDTO> getNotificationList() {
+        return notificationList;
+    }
+
+    public void setHomeUserDTO(HomeUserDTO userDTO) {
+        this.userDTO = userDTO;
+    }
+
+    public void setWishList(ArrayList<WishDTO> wishList) {
+        this.wishList = wishList;
+    }
+
+    public void setNotificationList(ArrayList<NotificationDTO> notificationList) {
         this.notificationList = notificationList;
     }
 
