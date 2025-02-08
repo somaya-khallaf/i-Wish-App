82685c389b5c734d50d175ca40f0042b1d3b25e3
somaya_khallaf <87336258+somaya-khallaf@users.noreply.github.com>
2025-02-09 01:04:56 +0200

    Update FriendRequestSL.java

--- Server/src/sl/FriendRequestSL.java
+++ Server/src/sl/FriendRequestSL.java
@@ -12,8 +12,18 @@ public class FriendRequestSL {
     static public void sendFriendRequest(String friendUsername, String userName) throws SQLException {
         FriendRequestDAO.sendFriendRequest(friendUsername, userName);
     }
-    static public int rejectFriendRequest(String friend, String userName)throws SQLException{return 0;}
-    static public int acceptFriendRequest(String friend, String userName) throws SQLException {return 0;}
+    static public int rejectFriendRequest(String friend, String userName) throws SQLException {
+        if (friend != null && !friend.isEmpty() && userName != null && !userName.isEmpty()) {
+            return FriendRequestDAO.rejectFriendRequest(friend, userName);
+        }
+        return 0;
+    }
+    static public int acceptFriendRequest(String friend, String userName) throws SQLException {
+        if (friend != null && !friend.isEmpty() && userName != null && !userName.isEmpty()) {
+            return FriendRequestDAO.acceptFriendRequest(friend, userName);
+        }
+        return 0;
+    }
     static public ArrayList<FriendDTO> getFriend(String friendName, String userName) throws SQLException {
         if(friendName != null && !friendName.isEmpty() && userName != null && !userName.isEmpty()  )
             return FriendRequestDAO.getFriend(friendName, userName);
