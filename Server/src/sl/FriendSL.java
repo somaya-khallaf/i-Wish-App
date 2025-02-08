b89a2184ead6c035bf04f4043f058dddb7ae2a06
somaya_khallaf <87336258+somaya-khallaf@users.noreply.github.com>
2025-02-09 01:05:30 +0200

    Update FriendSL.java

--- Server/src/sl/FriendSL.java
+++ Server/src/sl/FriendSL.java
@@ -9,7 +9,21 @@ import java.util.ArrayList;
 public class FriendSL {
 
     static public int addFriend(String friendUserName, String userName) throws SQLException {return 0;}
-    static public int removeFriend(String friendUserName, String userName) throws SQLException{return 0;}
-    static public ArrayList<FriendDTO> getFriendList(String userName) throws SQLException{return new ArrayList<>();}
+     static public int removeFriend(String friendUserName, String userName) throws SQLException {
+       if (friendUserName != null && !friendUserName.isEmpty() && userName != null && !userName.isEmpty()) {
+            return FriendDAO.removeFriend(friendUserName, userName);
+        }
+       System.out.println("Server response removeFriend SL null");
+        return 0;
+    }
+
+    static public ArrayList<FriendDTO> getFriendList(String userName) throws SQLException {
+        if (userName != null && !userName.isEmpty()) {
+            System.out.println("Server response getFriendList SL " + FriendDAO.getAllFriends(userName));
+            return FriendDAO.getAllFriends(userName);
+        }
+        System.out.println("Server response getFriendList SL null");
+        return null;
+    }
 
 }
