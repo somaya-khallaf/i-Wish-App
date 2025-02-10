f424b50e387759e94746270d1e2375e39338edf4
somaya_khallaf <87336258+somaya-khallaf@users.noreply.github.com>
2025-02-10 18:27:03 +0200

    create getWishListFriend function

--- Server/src/dal/WishDAO.java
+++ Server/src/dal/WishDAO.java
@@ -61,5 +61,36 @@ public class WishDAO {
         stmt.close();
         return wishList;
     }
+    static public ArrayList<WishDTO> getWishListFriend(String frienduserName) throws SQLException {
 
+        Database db = new Database();
+        Connection con = db.getConnection();
+        ArrayList<WishDTO> wishList = new ArrayList<>();
+        PreparedStatement findstmt = con.prepareStatement("select USERNAME from users where FULL_NAME = ? ");
+        findstmt.setString(1, frienduserName);
+        ResultSet friendUN = findstmt.executeQuery();
+        if (!friendUN.next()) {
+            con.rollback();
+            return wishList; // No deletion performed
+        }
+        String friendUsername = friendUN.getString("USERNAME");
+
+        PreparedStatement stmt = con.prepareStatement("WITH ContributionSums AS ( "
+                + "SELECT distinct WISH_ID, SUM(AMOUNT) over (partition by WISH_ID) AS TOTAL_CONTRIBUTIONS"
+                + " FROM  contributions) "
+                + "SELECT PT.PRODUCT_ID, WT.WISH_ID,PT.PRODUCT_NAME, PT.PRODUCT_PRICE, (PT.PRODUCT_PRICE - COALESCE(CS.TOTAL_CONTRIBUTIONS, 0)) "
+                + "AS REMAINING, WT.STATUS FROM wish_table WT "
+                + "JOIN product_table PT ON WT.PRODUCT_ID = PT.PRODUCT_ID "
+                + "LEFT JOIN  ContributionSums CS ON WT.WISH_ID = CS.WISH_ID "
+                + "WHERE WT.OWNER_NAME = ? ");
+        stmt.setString(1, friendUsername);
+        ResultSet rs = stmt.executeQuery();
+
+        while (rs.next()) {
+            wishList.add(new WishDTO(rs.getInt("PRODUCT_ID"), rs.getInt("WISH_ID"), rs.getString("PRODUCT_NAME"), rs.getDouble("PRODUCT_PRICE"), rs.getDouble("REMAINING"), rs.getString("STATUS")));            
+        }
+        db.close();
+        stmt.close();
+        return wishList;
+    }
 }
