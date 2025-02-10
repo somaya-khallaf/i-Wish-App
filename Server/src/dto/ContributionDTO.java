1fa3aadc77bf5a1bca33f6d8efad627da45f3bce
somaya_khallaf <87336258+somaya-khallaf@users.noreply.github.com>
2025-02-10 18:27:52 +0200

    Create ContributionDTO

--- Server/src/dto/ContributionDTO.java
+++ Server/src/dto/ContributionDTO.java
@@ -1,4 +1,49 @@
 package dto;
 
 public class ContributionDTO {
+
+    private int wish_id;
+    private String Contributer_name;
+    private double balance;
+    private double remaining;
+
+    public ContributionDTO(int wish_id, String Contributer_name, double amount, double remaining) {
+        this.wish_id = wish_id;
+        this.Contributer_name = Contributer_name;
+        this.balance = amount;
+        this.remaining = remaining;
+    }
+
+    public double getRemaining() {
+        return remaining;
+    }
+
+    public void setRemaining(double remaining) {
+        this.remaining = remaining;
+    }
+
+    public int getWish_id() {
+        return wish_id;
+    }
+
+    public String getContributer_name() {
+        return Contributer_name;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setWish_id(int wish_id) {
+        this.wish_id = wish_id;
+    }
+
+    public void setContributer_name(String Contributer_name) {
+        this.Contributer_name = Contributer_name;
+    }
+
+    public void setBalance(double amount) {
+        this.balance = amount;
+    }
+
 }
