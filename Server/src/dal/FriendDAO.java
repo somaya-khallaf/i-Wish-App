bb59222715a98f4ba9e938c6b3c699babe54f2ba
somaya_khallaf <87336258+somaya-khallaf@users.noreply.github.com>
2025-02-11 00:59:44 +0200

    Update removeFriend and getAllFriends function

--- Server/src/dal/FriendDAO.java
+++ Server/src/dal/FriendDAO.java
@@ -26,33 +26,18 @@ public class FriendDAO {
         Connection con = db.getConnection();
         con.setAutoCommit(false);
         try {
-            PreparedStatement stmt = con.prepareStatement("select USERNAME from users where FULL_NAME = ? ");
-            stmt.setString(1, friendUserName);
-            System.out.println(" removeFriend friendUN DAO" + stmt);
-            ResultSet friendUN = stmt.executeQuery();
-            if (!friendUN.next()) {
-                System.out.println("No user found with full name: " + friendUserName);
-                con.rollback();
-                return 0; // No deletion performed
-            }
-            String friendUsername = friendUN.getString("USERNAME");
-            System.out.println(" removeFriend friendUN DAO" + friendUsername);
-
             PreparedStatement deleteStmt1 = con.prepareStatement("delete from friend where (FRIENDNAME = ? and USERNAME = ?) ");
-            deleteStmt1.setString(1, friendUsername);
+            deleteStmt1.setString(1, friendUserName);
             deleteStmt1.setString(2, userName);
             int rs1 = deleteStmt1.executeUpdate();
 
-            System.out.println(" removeFriend friendUN DAO" + friendUsername);
             PreparedStatement deleteStmt2 = con.prepareStatement("delete from friend where (FRIENDNAME = ? and USERNAME = ?)");
             deleteStmt2.setString(1, userName);
-            deleteStmt2.setString(2, friendUsername);
+            deleteStmt2.setString(2, friendUserName);
 
             int rs2 = deleteStmt2.executeUpdate();
 
             con.commit();
-
-            System.out.println("Result from DAO: " + rs1 + rs2);
             return rs1 + rs2;
         } catch (SQLException e) {
             con.rollback();
@@ -68,13 +53,13 @@ public class FriendDAO {
         ArrayList<FriendDTO> requests = new ArrayList<>();
         Database db = new Database();
         Connection con = db.getConnection();
-        PreparedStatement stmt = con.prepareStatement("select U.FULL_NAME from friend fr join users u on FR.FRIENDNAME=U.USERNAME  where FR.USERNAME = ? ");
+        PreparedStatement stmt = con.prepareStatement("select U.FULL_NAME , U.USERNAME from friend fr join users u on FR.FRIENDNAME=U.USERNAME  where FR.USERNAME = ? ");
         stmt.setString(1, userName);
         ResultSet rs = stmt.executeQuery();
         while (rs.next()) {
-            FriendDTO temp = new FriendDTO(rs.getString("FULL_NAME"));
+            FriendDTO temp = new FriendDTO(rs.getString("USERNAME"), rs.getString("FULL_NAME"));
             requests.add(temp);
-            System.out.println(rs.getString("FULL_NAME"));
+            System.out.println(rs.getString("FULL_NAME") + " " + rs.getString("USERNAME"));
         }
         db.close();
         stmt.close();
