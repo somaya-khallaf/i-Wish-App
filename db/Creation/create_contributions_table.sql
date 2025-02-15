37313e26d9d8f07fa7773f4336875362f7b50e75
Mohamed Ashraf <93071951+TmohamedashrafT@users.noreply.github.com>
2025-02-15 22:53:10 +0200

    Update create_contributions_table.sql

--- db/Creation/create_contributions_table.sql
+++ db/Creation/create_contributions_table.sql
@@ -1,7 +1,7 @@
 create table contributions (
     Contributer_name varchar2(50) not null ,
     wish_id number(8) not null,
-    amount number(8) not null,
+    amount number(8,3) not null,
     constraint wish_contribute_pk primary key (Contributer_name, wish_id),
     constraint Contributer_name_fk foreign key  (Contributer_name) references Users(Username),
     constraint wish_id_fk foreign key (wish_id) references wish_table(wish_id)
